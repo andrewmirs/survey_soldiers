d66bb7a2d1769f14c597883ae0563a01f97bc7e7
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-05-28 15:35:48 -0700

    full bare bones authentication flow finished with logout

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+// Let express know to care about cookies
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        keys: [keys.cookieKey] // encrypts the cookie
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Auth routes function invoked with app as an argument
 require('./routes/authRoutes')(app);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,4 +14,15 @@ module.exports = (app) => {
         '/auth/google/callback', 
         passport.authenticate('google')
     );
-}
+
+    // Log user out
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    // Test authentication by sending back user data
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
+};
